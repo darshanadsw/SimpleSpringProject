bcb0ec9e6468982b402308c74e5d03f1a7102faa
darshanadsw <darshanadsw@gmail.com>
2018-04-27 07:47:23 -0400

    modifications

--- src/com/springframework/chapter2/autowiring/PayrollSystem.java
+++ src/com/springframework/chapter2/autowiring/PayrollSystem.java
@@ -1,8 +1,19 @@
 package com.springframework.chapter2.autowiring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class PayrollSystem {
 
 	public static void main(String... args) {
 		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		EmployeeService empServiceA = (EmployeeService)context.getBean("employeeService");
+		empServiceA.setMessage("Message by Service A");
+		System.out.println(empServiceA.getMessage());
+		
+		EmployeeService empServiceB = (EmployeeService)context.getBean("employeeService");		
+		System.out.println(empServiceB.getMessage());
 	}
 }
